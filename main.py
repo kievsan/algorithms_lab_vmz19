e4a9a29609006bee2f1444c530c9b8d5a45f59d3
Sergey Kievskiy <kievsan@mail.ru>
2022-07-03 00:54:23 +0300

    Edit files

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 from pprint import pprint
 import time
 import json
+import csv
 import lab1
 
 
@@ -18,6 +19,17 @@ def write_to_json_file(filename, results):
         print('Sorry, the results are not saved to a json file...')
 
 
+def write_to_csv_file(filename, results):
+    try:
+        with open(filename, 'w', newline="", encoding='utf-8') as f:
+            data_writer = csv.writer(f, delimiter=',')
+            data_writer.writerows(results)
+        print(f'The results are saved to {filename}\n')
+    except OSError as other:
+        print(f'There were problems opening the file {filename}: \n\t{other}\n')
+        print('Sorry, the results are not saved to a csv file...')
+
+
 def write_to_log_file(filename, results):
     try:
         with open(filename, 'a', encoding='utf-8') as f:
@@ -25,7 +37,7 @@ def write_to_log_file(filename, results):
             f.write(str(time.time()) + ':\n')
             for data in results:
                 f.write(data + '\n')
-        print(f'The results are saved to {filename}\n')
+        print(f'The results are saved to {filename}')
     except OSError as other:
         print(f'There were problems opening the file {filename}: \n\t{other}\n')
         print('Sorry, the results are not saved to a log file...')
@@ -34,26 +46,29 @@ def write_to_log_file(filename, results):
 if __name__ == '__main__':
     # Лабораторная работа №1
     #
+    header = ['array size', 'Odd-Even Sort time', 'Heap Sort time']
+    csv_resultsLab1 = [header]
+    json_resultsLab1 = []
     log_resultsLab1 = []
-    list_resultsLab1 = []
-    header = ['item', 'array size', 'Odd-Even Sort time', 'Heap Sort time']
-    header_line = '{:>10} {:>18} {:>18}'.format(header[1], header[2], header[3])
+    header_line = '{:>10} {:>18} {:>18}'.format(header[0], header[1], header[2])
     log_resultsLab1.append(header_line)
     print(header_line)
 
-    for size in range(5000, 100001, 5000):
-        record = {header[1]: size,
-                  header[2]: lab1.algorithm_OddEvenSort(size),
-                  header[3]: lab1.algorithm_maxHeapSort(size)}
-        list_resultsLab1.append(record)
-        result = '{:>10} {:>18f} {:>18f}'.format(
-            record[header[1]], record[header[2]], record[header[3]])
+    for size in range(50, 1001, 50):
+        csv_ = [size,
+                lab1.algorithm_OddEvenSort(size),
+                lab1.algorithm_maxHeapSort(size)]
+        csv_resultsLab1.append(csv_)
+        json_ = {header[0]: size,
+                 header[1]: csv_[1],
+                 header[2]: csv_[2]}
+        json_resultsLab1.append(json_)
+        result = '{:>10} {:>18f} {:>18f}'.format(csv_[0], csv_[1], csv_[2])
         log_resultsLab1.append(result)
         print(result)
 
     write_to_log_file('Lab1_algorithms.log', log_resultsLab1)
+    write_to_csv_file('Lab1_algorithms.csv', csv_resultsLab1)
     if input('Do you want to save the results to json file? (Y/N) >>'
              ) in ['y', 'Y', 'н', 'Н']:
-        write_to_json_file('Lab1_algorithms.json', list_resultsLab1)
-
-
+        write_to_json_file('Lab1_algorithms.json', json_resultsLab1)
