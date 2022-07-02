2cfdb6a1892ab1cac13056b81ede6f2339950243
Sergey Kievskiy <kievsan@mail.ru>
2022-07-03 02:03:37 +0300

    Edit files

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ def write_to_json_file(filename, results):
 def write_to_csv_file(filename, results):
     try:
         with open(filename, 'w', newline="", encoding='utf-8') as f:
-            data_writer = csv.writer(f, delimiter=',')
+            data_writer = csv.DictWriter(f, fieldnames=results[0].keys(), delimiter=',')
+            data_writer.writeheader()
             data_writer.writerows(results)
         print(f'The results are saved to {filename}\n')
     except OSError as other:
@@ -47,28 +48,24 @@ if __name__ == '__main__':
     # Лабораторная работа №1
     #
     header = ['array size', 'Odd-Even Sort time', 'Heap Sort time']
-    csv_resultsLab1 = [header]
-    json_resultsLab1 = []
+    list_resultsLab1 = []
     log_resultsLab1 = []
     header_line = '{:>10} {:>18} {:>18}'.format(header[0], header[1], header[2])
     log_resultsLab1.append(header_line)
     print(header_line)
 
     for size in range(50, 1001, 50):
-        csv_ = [size,
-                lab1.algorithm_OddEvenSort(size),
-                lab1.algorithm_maxHeapSort(size)]
-        csv_resultsLab1.append(csv_)
-        json_ = {header[0]: size,
-                 header[1]: csv_[1],
-                 header[2]: csv_[2]}
-        json_resultsLab1.append(json_)
-        result = '{:>10} {:>18f} {:>18f}'.format(csv_[0], csv_[1], csv_[2])
+        dict_ = {header[0]: size,
+                 header[1]: lab1.algorithm_OddEvenSort(size),
+                 header[2]: lab1.algorithm_maxHeapSort(size)}
+        list_resultsLab1.append(dict_)
+        result = '{:>10} {:>18f} {:>18f}'.format(
+            dict_[header[0]], dict_[header[1]], dict_[header[2]])
         log_resultsLab1.append(result)
         print(result)
 
     write_to_log_file('Lab1_algorithms.log', log_resultsLab1)
-    write_to_csv_file('Lab1_algorithms.csv', csv_resultsLab1)
+    write_to_csv_file('Lab1_algorithms.csv', list_resultsLab1)
     if input('Do you want to save the results to json file? (Y/N) >>'
              ) in ['y', 'Y', 'н', 'Н']:
-        write_to_json_file('Lab1_algorithms.json', json_resultsLab1)
+        write_to_json_file('Lab1_algorithms.json', list_resultsLab1)
